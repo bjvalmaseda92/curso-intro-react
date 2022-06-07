256ef31b5088f8f01d72fe2e5e3ad2423bf143af
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-06-07 17:09:42 -0400

    feat: ✨ add search and counter functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
 // import './App.css';
 
-const todos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el cursso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
-  { text: 'LALALALAA', completed: false },
+const defaulutTodos = [
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tomar el cursso de intro a React", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+  { text: "LALALALAA", completed: true },
 ];
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState("");
+  let searchedTodos = [];
+
+  if (searchValue.length >= 1) {
+    searchedTodos = defaulutTodos.filter((todo) => {
+      const searchText = searchValue.toLowerCase();
+      const todoText = todo.text.toLocaleLowerCase();
+      return todoText.includes(searchText);
+    });
+  } else {
+    searchedTodos = defaulutTodos;
+  }
+
+  const totalTodos = searchedTodos.length;
+  const completedTodos = searchedTodos.filter((todo) => todo.completed).length;
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter total={totalTodos} completed={completedTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
