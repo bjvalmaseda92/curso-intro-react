e79323fdc213acf38aef49070cdb8b8a9e8d4026
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-06-07 17:45:04 -0400

    refactor: ⛑️ fixed react 18 React.render warning

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import App from "./App";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(<App />);
